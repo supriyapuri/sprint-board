986f050cbff37aadaf02068d31530da0566c174d
supriyapuri <supriyapuri11@gmail.com>
2020-06-09 00:16:59 -0700

    Updates

---
+++ precache-manifest.15601b7a883c4a6f4c4e43f3afd78def.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad4b76f55f7ca974d617ce6f25e56e10",
+    "url": "/sprint-board/index.html"
+  },
+  {
+    "revision": "6d9feee8f152abe49a13",
+    "url": "/sprint-board/static/css/main.c5d4be17.chunk.css"
+  },
+  {
+    "revision": "c2c671d7dbfb3be57cb3",
+    "url": "/sprint-board/static/js/2.179951b8.chunk.js"
+  },
+  {
+    "revision": "5a064244f682781762df8bff5d572397",
+    "url": "/sprint-board/static/js/2.179951b8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d9feee8f152abe49a13",
+    "url": "/sprint-board/static/js/main.0a942c84.chunk.js"
+  },
+  {
+    "revision": "519bdb7b2a9d09e15916",
+    "url": "/sprint-board/static/js/runtime-main.44a3d6ce.js"
+  }
+]);
